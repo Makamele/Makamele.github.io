f2ed3ab6ddef27b62cee4c74b384d786c17643ec
Makamele <tjabadiamakamele@gamil.com>
2022-06-16 18:45:38 +0200

    particles

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ window.onload = function() {
     Particles.init({
       selector: ".particles",
       color: ['#030700','#e90d0d'],
-      connectParticles: true,
-      maxParticles: 200,
-    //   sizeVariations: 50,
+      connectParticles: false,
+      maxParticles: 50,
+      sizeVariations: 30,
       speed: 4,
       
     });
