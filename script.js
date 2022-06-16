1451b7bd20e1c669c72034880e13c40763333e32
Makamele <tjabadiamakamele@gamil.com>
2022-06-16 18:56:16 +0200

    ..

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ window.onload = function() {
       color: ['#030700','#e90d0d'],
       connectParticles: false,
       maxParticles: 50,
-      sizeVariations: 30,
-      speed: 4,
+      sizeVariations: 20,
+      speed: 2,
       
     });
 };
